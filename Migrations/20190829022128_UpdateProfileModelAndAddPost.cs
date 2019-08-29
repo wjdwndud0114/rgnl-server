1b3b356794571b640df9d66dc5a74ee2ee143811
wjdwndud0114 <wjdwndud0114@yahoo.com>
2019-08-28 19:25:16 -0700

    Migration for Adding Post, modify Profile

---
+++ Migrations/20190829022128_UpdateProfileModelAndAddPost.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace rgnl_server.Migrations
+{
+    public partial class UpdateProfileModelAndAddPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContactInformation",
+                table: "Profile",
+                newName: "Address");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Profile",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Profile",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserId",
+                table: "AspNetRoles",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Post_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoles_AppUserId",
+                table: "AspNetRoles",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_AppUserId",
+                table: "Post",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_AppUserId",
+                table: "AspNetRoles",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_AppUserId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoles_AppUserId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Profile");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Profile");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "AspNetRoles");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Profile",
+                newName: "ContactInformation");
+        }
+    }
+}
--- Models/Entities/Post.cs
+++ Models/Entities/Post.cs
@@ -7,8 +7,8 @@ namespace rgnl_server.Models.Entities
         public int PostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public DateTimeOffset CreatedDate { get; set; }
-        public DateTimeOffset? UpdatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
         public int AppUserId { get; set; }
         public virtual AppUser AppUser { get; set; }
     }
